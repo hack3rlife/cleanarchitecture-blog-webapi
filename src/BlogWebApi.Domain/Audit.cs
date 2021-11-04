a7ee0a5b907003bfc4d3a2d86a3962dff6bf2676
hack3rlife <13023914+hack3rlife@users.noreply.github.com>
2021-11-04 15:28:30 -0700

    Adding Domain Layer (#3)
    Adding Build Status badge
    
    Co-authored-by: hack3rlife <admin@hack3rlife.com>

---
+++ src/BlogWebApi.Domain/Audit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogWebApi.Domain
+{
+    public class Audit
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CreatedDate { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public string CreatedBy { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? LastUpdate { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public string UpdatedBy { get; set; }
+
+    }
+}
---
+++ src/BlogWebApi.Domain/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogWebApi.Domain
+{
+    public class Comment : Audit
+    {
+        public Guid CommentId { get; set; }
+        public string CommentName { get; set; }
+        public string Email { get; set; }
+        public Guid PostId { get; set; }
+    }
+}
