40a53b73e5768c2f8bc65710ee8403acdef8657c
hack3rlife <admin@hack3rlife.com>
2021-11-06 23:29:29 -0700

    Adding more Unit Tests to PostService

--- tests/Application.UnitTest/Services/PostService/PostServiceUpdateTests.cs
+++ tests/Application.UnitTest/Services/PostService/PostServiceUpdateTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Application.UnitTest.Builders;
+using BlogWebApi.Domain;
 using Moq;
 using Xunit;
 
@@ -44,6 +45,80 @@ namespace Application.UnitTest.Services.PostService
             MockPostRepository.MockVerifyUpdateAsync(updatePost, Times.Never());
         }
 
+        [Fact(DisplayName = "Update_NullPost_ThrowsArgumentNullException")]
+        public async Task Update_NullPost_ThrowsArgumentNullException()
+        {
+            //Arrange
+
+            //Act
+            async Task Act() => await PostService.Update(null);
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(Act);
+
+            // Assert
+            Assert.Equal("The post cannot be null. (Parameter 'post')", exception.Message);
+        }
+
+        [Fact(DisplayName = "Update_EmptyPostId_ThrowsArgumentNullException")]
+        public async Task Update_EmptyPostId_ThrowsArgumentNullException()
+        {
+            //Arrange
+            var post = PostBuilder.WithoutPostId();
+
+            //Act
+            async Task Act() => await PostService.Update(post);
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(Act);
+
+            // Assert
+            Assert.Equal("The postId cannot be empty Guid. (Parameter 'post')", exception.Message);
+        }
+
+        [Theory(DisplayName = "Update_WithEmptyOrNullName_ThrowsArgumentNullException")]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task Update_WithEmptyOrNullName_ThrowsArgumentNullException(string name)
+        {
+            //Arrange
+            var post = PostBuilder.Default();
+            post.PostName = name;
+
+            //Act
+            async Task Act() => await PostService.Update(post);
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(Act);
+
+            // Assert
+            Assert.Equal("The post name cannot be null or empty. (Parameter 'post')", exception.Message);
+        }
+
+        [Theory(DisplayName = "Update_WithEmptyOrNullText_ThrowsArgumentNullException")]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task Update_WithEmptyOrNullText_ThrowsArgumentNullException(string text)
+        {
+            //Arrange
+            var post = PostBuilder.Default();
+            post.Text = text;
+
+            //Act
+            async Task Act() => await PostService.Update(post);
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(Act);
+
+            // Assert
+            Assert.Equal("The post text cannot be null or empty. (Parameter 'post')", exception.Message);
+        }
+
+        [Fact(DisplayName = "Update_EmptyBlogId_ThrowsArgumentNullException")]
+        public async Task Update_EmptyBlogId_ThrowsArgumentNullException()
+        {
+            //Arrange
+            var post = PostBuilder.WithoutBlogId();
+
+            //Act
+            async Task Act() => await PostService.Update(post);
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(Act);
+
+            // Assert
+            Assert.Equal("The blogId cannot be empty Guid. (Parameter 'post')", exception.Message);
+        }
 
         [Fact(DisplayName = "Update_PostUsingLongName_ThrowsArgumentOutOfRangeException")]
         public async Task Update_PostUsingLongName_ThrowsArgumentOutOfRangeException()
