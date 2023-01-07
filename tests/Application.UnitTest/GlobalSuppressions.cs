c21e01e2073bfc4d0a13c3b0969b3b0a0673b218
hack3rlife <hack3rlifeio@gmail.com>
2023-01-07 14:01:04 -0800

    Updating non-Microsoft Nuget Packages

---
+++ tests/Application.UnitTest/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "xUnit1012:Null should not be used for value type parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Application.UnitTest.Services.PostService.PostServiceAddTests.Add_PostWithEmptyOrNullBlogId_ThrowsBadRequestException(System.Guid)~System.Threading.Tasks.Task")]
